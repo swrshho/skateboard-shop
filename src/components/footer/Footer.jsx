7c28dba63c79bfa52356b6ce28f394f05dd858dd
Soroush Hosseini <swrshho@gmail.com>
2022-08-23 16:42:33 +0430

    Fix hover for nav & footer links and socialBTN

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,71 +1,68 @@
-import { Link } from "react-router-dom";
-import CTA from "../../components/cta/CTA";
-import SocialLinks from "../socialLinks/SocialLinks";
-import { footerNavData } from "./data";
-import Logo from "../../assets/logo/Logo";
+import { Link } from 'react-router-dom'
+import CTA from '../../components/cta/CTA'
+import SocialLinks from '../socialLinks/SocialLinks'
+import { footerNavData } from './data'
+import Logo from '../../assets/logo/Logo'
 
 const Footer = () => {
-  return (
-    <div className="flex h-screen flex-col items-center justify-between bg-velvet-red p-10 pb-2 font-poppins sm:h-512px">
-      <h1 className="min-w-450px max-w-512px text-center font-anton text-6xl uppercase text-white">
-        get the latest news about us
-      </h1>
+	return (
+		<div className='flex h-screen flex-col items-center justify-between bg-velvet-red p-10 pb-2 font-poppins sm:h-512px'>
+			<h1 className='min-w-450px max-w-512px text-center font-anton text-6xl uppercase text-white'>
+				get the latest news about us
+			</h1>
 
-      <div className="flex w-4/5 max-w-330px flex-col items-center justify-between sm:max-w-512px">
-        <form className="sm:flex">
-          <input
-            className="h-12 w-full rounded-md p-2 text-black sm:rounded-r-none"
-            type="email"
-            placeholder="Your E-mail Address..."
-          />
-          <CTA
-            className="hidden rounded-l-none sm:inline-block"
-            type="primary"
-            path="./shop"
-            rounded
-          >
-            submit
-          </CTA>
-        </form>
+			<div className='flex w-4/5 max-w-330px flex-col items-center justify-between sm:max-w-512px'>
+				<form className='sm:flex'>
+					<input
+						className='h-12 w-full rounded-md p-2 text-black sm:rounded-r-none'
+						type='email'
+						placeholder='Your E-mail Address...'
+					/>
+					<CTA
+						className='hidden rounded-l-none sm:inline-block'
+						type='primary'
+						path='./shop'
+						rounded>
+						submit
+					</CTA>
+				</form>
 
-        <CTA
-          className="mt-2 w-full text-center sm:hidden"
-          type="primary"
-          path="./shop"
-          rounded
-        >
-          submit
-        </CTA>
-      </div>
+				<CTA
+					className='mt-2 w-full text-center sm:hidden'
+					type='primary'
+					path='./shop'
+					rounded>
+					submit
+				</CTA>
+			</div>
 
-      <div className="sm:min-w-92 sm:max-w-sm">
-        <ul
-          className="flex h-full w-full flex-col items-center justify-around
+			<div className='sm:min-w-92 sm:max-w-sm'>
+				<ul
+					className='flex h-full w-full flex-col items-center justify-around
 						font-poppins text-xl capitalize text-white
-						sm:flex-row"
-        >
-          {footerNavData.map((link, idx) => (
-            <li className="mb-3 last:mb-0 sm:m-0" key={idx}>
-              <Link to={link.path}>{link.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+						sm:flex-row'>
+					{footerNavData.map((link, idx) => (
+						<li className='underline-hover mb-3 last:mb-0 sm:m-0' key={idx}>
+							<Link to={link.path}>{link.title}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
 
-      <SocialLinks className="sm:hidden" />
+			<SocialLinks className='sm:hidden' />
 
-      <div className="flex w-full flex-col items-center justify-between">
-        <span className="mb-4 inline-block h-px w-full bg-white" />
-        <div className="flex w-11/12 flex-col items-center justify-between sm:flex-row">
-          <Logo />
-          <p className="text-center font-poppins capitalize text-white">
-            © all rights reserved
-          </p>
-          <SocialLinks className="hidden sm:flex" />
-        </div>
-      </div>
-    </div>
-  );
-};
+			<div className='flex w-full flex-col items-center justify-between'>
+				<span className='mb-4 inline-block h-px w-full bg-white' />
+				<div className='flex w-11/12 flex-col items-center justify-between sm:flex-row'>
+					<Logo />
+					<p className='text-center font-poppins capitalize text-white'>
+						© all rights reserved
+					</p>
+					<SocialLinks className='hidden sm:flex' />
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default Footer;
+export default Footer
--- src/components/socialBTN/SocialBTN.jsx
+++ src/components/socialBTN/SocialBTN.jsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
-import { IconContext } from "react-icons";
+import { Link } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { useState } from 'react'
 
 const SocialBTN = ({ path, icon }) => {
-  return (
-    <div className="rounded-full bg-white p-2">
-      <IconContext.Provider value={{ color: "black", size: "1em" }}>
-        <Link to={path}>{icon}</Link>
-      </IconContext.Provider>
-    </div>
-  );
-};
+	const [hover, setHover] = useState(false)
 
-export default SocialBTN;
+	return (
+		<IconContext.Provider
+			value={{ color: hover ? 'white' : 'black', size: '1em' }}>
+			<Link to={path}>
+				<div
+					onMouseEnter={() => setHover(true)}
+					onMouseLeave={() => setHover(false)}
+					className='rounded-full bg-white p-2 transition-all hover:bg-black'>
+					{icon}
+				</div>
+			</Link>
+		</IconContext.Provider>
+	)
+}
+
+export default SocialBTN
